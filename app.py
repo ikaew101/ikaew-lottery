8ba643db33a952e16e59f158bd1b041cd0d12b50
ikaew32bit <ikaew32bit@gmail.com>
2026-01-02 11:10:55 +0700

    add line bot

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from services.bot_logic import get_prediction_message
+from src.bot_logic import get_prediction_message
 import os
 
 app = Flask(__name__)
